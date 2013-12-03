d55d9779068e42cbc96ce1945a96324d9cc44a33
Andy Terkik <aterkik@gmail.com>
2013-12-02 21:30:26 -0500

    clean up

--- jquery.tweetHighlighted.js
+++ jquery.tweetHighlighted.js
@@ -5,26 +5,30 @@
  */
 (function ($) {
     $.fn.tweetHighlighted = function(options) {
-        var settings = {};
+        var settings = {}, classes;
         options = options || {};
-        // minimum length of the selected text for the tweet button to appear
-        settings.minLength = options.minLength || 8;
-        // maximum length of the selected text for the tweet button to appear
-        settings.maxLength = options.maxLength || 144 * 2;
-        // twitter via
-        settings.via = options.via || '';
+
+        // html DOM element to use as a button
         settings.node = options.node || '<button type="button">Tweet</button>';
-        // css class
+        // css class to add to the html node
         settings.cssClass = options.cssClass || 'tweet-me';
-        // extra content to attach (mostly the current URL)
+        // minimum length of the selected text for the tweet button to appear
+        settings.minLength = options.minLength || 1;
+        // maximum length of the selected text for the tweet button to appear
+        settings.maxLength = options.maxLength || 144 * 4;
+        // extra content to attach (mostly used to add URLs)
         settings.extra = options.extra || '';
+        // twitter via handle
+        // (basically appends 'via @twitterhandle' to the tweet)
+        settings.via = options.via || null;
 
-        var classes = settings.cssClass.split(' ').filter(
-                                                  function(item) {
-                                                    return item.length;
-                                                  });
-        settings.selector = '.' + classes.join('.');
+        // get an array of classes filtering out empty whitespaces
+        classes = settings.cssClass.split(' ').filter(function(item) {
+                                                        return item.length;
+                                                      });
+        settings._selector = '.' + classes.join('.');
 
+        // event that fires when any non-empty text is selected
         var onTextSelect = function(selector, callback) {
             function getSelectedText() {
               if (window.getSelection) {
@@ -50,21 +54,31 @@
                 e.stopPropagation();
               }
               else
-                $(settings.selector).fadeOut(500).remove();
+                $(settings._selector).fadeOut(500).remove();
             });
         };
 
+        var getTweetURL = function(text, extra, via) {
+            var url = 'https://twitter.com/intent/tweet?text=';
+            url += encodeURIComponent(text);
+
+           if (extra)
+                url += encodeURIComponent(' ' + extra);
+
+            if (via)
+                url += '&via=' + via;
+
+            return url;
+        };
+
         onTextSelect(this, function(e, text) {
-            var url,
-                btnExists = $(settings.selector).length;
+            var btnExists = $(settings._selector).length, url;
 
             if (btnExists || text.length > settings.maxLength
                 || text.length < settings.minLength)
                 return;
 
-            url = 'https://twitter.com/intent/tweet?via=' + settings.via;
-            url += '&text=' + encodeURIComponent(text) + ' ' + settings.extra;
-
+            url = getTweetURL(text, settings.extra, settings.via);
             $(settings.node).addClass(settings.cssClass
              ).offset({top: e.pageY,
                        left: e.pageX}
@@ -73,7 +87,7 @@
              ).appendTo('body'
              ).fadeIn(500
              ).click(function(e) {
-                 $(settings.selector).fadeOut(500).remove();
+                 $(settings._selector).fadeOut(500).remove();
                  window.open(url, '_blank', 'width=400,height=400,toolbar=0,location=0');
              });
         });      
